37710d84121b2d96124cef36db6151f16284e709
Weis <harmonia.rainshard@gmail.com>
2022-11-27 11:30:48 +0700

    Change UI and Vendor v.5.85"

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,7 @@
   <link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet">
   <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
   <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  {{-- <link href="{{ asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet"> --}}
+  <link href="{{ asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
   <link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
   {{-- <link href="{{ asset('vendor/remixicon/remixicon.css') }}" rel="stylesheet"> --}}
   {{-- <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet"> --}}
