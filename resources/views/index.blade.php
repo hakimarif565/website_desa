f05e4580c2bd7d65f7df98e0d30487fc9343d789
Weis <harmonia.rainshard@gmail.com>
2022-11-27 11:42:40 +0700

    Profile Page Revision v.6

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -657,11 +657,8 @@
             <h4>Sosial Media</h4>
             <p>Temukan Kampung Dinamo Surabaya pada Sosial Media Dibawah Ini</p>
             <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="https://www.instagram.com/kknngagelrejo/" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
               <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
             </div>
           </div>
 
