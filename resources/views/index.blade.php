9d438ce4fb5bd43c7730a57cdb56fb5e3a4f1378
Weis <harmonia.rainshard@gmail.com>
2022-11-27 11:28:54 +0700

    Change UI and Vendor v.5.8"

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,8 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{ asset('img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+  {{-- <link href="{{ asset('img/favicon.png') }}" rel="icon">
+  <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon"> --}}
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
@@ -20,20 +20,14 @@
   <link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet">
   <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
   <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  {{-- <link href="{{ asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet"> --}}
   <link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+  {{-- <link href="{{ asset('vendor/remixicon/remixicon.css') }}" rel="stylesheet"> --}}
+  {{-- <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet"> --}}
 
   <!-- Template Main CSS File -->
   <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: Arsha - v4.9.1
-  * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
@@ -76,7 +70,7 @@
           </div>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-          <img src="{{ asset('img/desa/icon_desa.png') }}" class="img-fluid animated" alt="">
+          <img src="{{ asset('img/desa/icon_desa.png') }}" class="img-fluid" alt="">
         </div>
       </div>
     </div>
@@ -362,25 +356,25 @@
         <div class="row mt-4">
           <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
             <div class="icon-box">
-              <div class="icon"><img src="https://dummyimage.com/200x200/" /></i></div>
+              <div class="icon"><img height="200" width="200" src="{{ asset('img/event/event1.jpg') }}" /></i></div>
               <h4><a>Kegiatan & Event</a></h4>
             </div>
           </div>
           <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
             <div class="icon-box">
-              <div class="icon"><img src="https://dummyimage.com/200x200/" /></i></div>
+              <div class="icon"><img height="200" width="200" src="{{ asset('img/event/event2.jpg') }}" /></i></div>
               <h4><a>Kegiatan & Event</a></h4>
             </div>
           </div>
           <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
             <div class="icon-box">
-              <div class="icon"><img src="https://dummyimage.com/200x200/" /></div>
+              <div class="icon"><img height="200" width="200" src="{{ asset('img/event/event3.jpg') }}" /></div>
               <h4><a>Kegiatan & Event</a></h4>
             </div>
           </div>
           <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0">
             <div class="icon-box">
-              <div class="icon"><img src="https://dummyimage.com/200x200/" /></div>
+              <div class="icon"><img height="200" width="200" src="{{ asset('img/event/event4.jpg') }}" /></div>
               <h4><a>Kegiatan & Event</a></h4>
             </div>
           </div>
@@ -388,37 +382,29 @@
         <div class="row mt-4">
             <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
               <div class="icon-box">
-                <div class="icon"><img src="https://dummyimage.com/200x200/" /></i></div>
-                <h4><a href="#">Kegiatan & Event</a></h4>
-
+                <div class="icon"><img height="200" width="200" src="{{ asset('img/event/event5.jpg') }}" /></i></div>
+                <h4><a>Kegiatan & Event</a></h4>
               </div>
             </div>
-
             <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
               <div class="icon-box">
-                <div class="icon"><img src="https://dummyimage.com/200x200/" /></i></div>
-                <h4><a href="#">Kegiatan & Event</a></h4>
-
+                <div class="icon"><img height="200" width="200" src="{{ asset('img/event/event6.jpg') }}" /></i></div>
+                <h4><a>Kegiatan & Event</a></h4>
               </div>
             </div>
-
             <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
               <div class="icon-box">
-                <div class="icon"><img src="https://dummyimage.com/200x200/" /></i></div>
-                <h4><a href="#">Kegiatan & Event</a></h4>
-
+                <div class="icon"><img height="200" width="200" src="{{ asset('img/event/event7.jpg') }}" /></i></div>
+                <h4><a>Kegiatan & Event</a></h4>
               </div>
             </div>
-
             <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0">
               <div class="icon-box">
-                <div class="icon"><img src="https://dummyimage.com/200x200/" /></i></div>
-                <h4><a href="#">Kegiatan & Event</a></h4>
-
+                <div class="icon"><img height="200" width="200" src="{{ asset('img/event/event8.jpg') }}" /></i></div>
+                <h4><a>Kegiatan & Event</a></h4>
               </div>
             </div>
           </div>
-
         <div class="row mt-4 col-12">
             <div class="media container-fluid">
                 <div class="media-body col-12">
@@ -427,7 +413,6 @@
                 </div>
             </div>
         </div>
-
       </div>
     </section><!-- End Dokumentasi Section -->
 
@@ -705,10 +690,10 @@
   <script src="{{ asset('vendor/aos/aos.js') }}"></script>
   <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
   <script src="{{ asset('vendor/glightbox/js/glightbox.min.js') }}"></script>
-  <script src="{{ asset('vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+  {{-- <script src="{{ asset('vendor/isotope-layout/isotope.pkgd.min.js') }}"></script> --}}
   <script src="{{ asset('vendor/swiper/swiper-bundle.min.js') }}"></script>
   <script src="{{ asset('vendor/waypoints/noframework.waypoints.js') }}"></script>
-  <script src="{{ asset('vendor/php-email-form/validate.js') }}"></script>
+  {{-- <script src="{{ asset('vendor/php-email-form/validate.js') }}"></script> --}}
 
   <!-- Template Main JS File -->
   <script src="{{ asset('js/main.js') }}"></script>
