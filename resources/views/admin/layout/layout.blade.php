a47a802b7a1dcf9667e1117645e51f42fc66a1bd
Vio Kartiko <viokartiko21@gmail.com>
2022-12-10 21:07:42 +0700

    admin

--- resources/views/admin/layout/layout.blade.php
+++ resources/views/admin/layout/layout.blade.php
@@ -42,7 +42,7 @@
             <div class="logo-header">
 
                 <a href="index.html" class="logo">
-                    <img src="/assets/img/dynamo.png" alt="navbar brand" class="navbar-brand">
+                    <img src="/assets/img/dynamo.png" width="120" height="50" alt="navbar brand" class="navbar-brand">
                 </a>
                 <button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon">
